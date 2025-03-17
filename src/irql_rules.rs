a00e0e0e29979818c7a6efd5cfece4935a937950
antoniopotra <antonio.potra@gmail.com>
2025-03-17 18:23:22 +0200

    Changed the way annotations are obtained for method vs function calls.

--- src/irql_rules.rs
+++ src/irql_rules.rs
@@ -25,18 +25,38 @@ impl_lint_pass!(IrqlRules<'_> => [IRQL_RULES]);
 
 impl<'tcx> LateLintPass<'tcx> for IrqlRules<'tcx> {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx rustc_hir::Expr<'_>) {
-        match &expr.kind {
-            rustc_hir::ExprKind::Call(_, _) | rustc_hir::ExprKind::MethodCall(_, _, _, _) => {
-                let Some(function_def_id) = cx.typeck_results().type_dependent_def_id(expr.hir_id)
-                else {
-                    return;
-                };
-                let caller_def_id = cx.tcx.hir().enclosing_body_owner(expr.hir_id).to_def_id();
-
-                let function_irql = self.cx.irql_annotation(function_def_id);
-                let caller_irql = self.cx.irql_annotation(caller_def_id);
+        let function_def_id = match &expr.kind {
+            rustc_hir::ExprKind::Call(func, _) => {
+                if let rustc_hir::ExprKind::Path(rustc_hir::QPath::Resolved(_, path)) = func.kind {
+                    path.res.opt_def_id()
+                } else {
+                    None
+                }
             }
-            _ => {}
+            rustc_hir::ExprKind::MethodCall(_, _, _, _) => {
+                cx.typeck_results().type_dependent_def_id(expr.hir_id)
+            }
+            _ => None,
+        };
+
+        let Some(function_def_id) = function_def_id else {
+            return;
+        };
+        let caller_def_id = cx.tcx.hir().enclosing_body_owner(expr.hir_id).to_def_id();
+
+        let function_irql = self.cx.irql_annotation(function_def_id);
+        let caller_irql = self.cx.irql_annotation(caller_def_id);
+
+        if let Some(on_return_value) = function_irql.on_return_value
+            && let Some(permanent_requirement) = caller_irql.permanent_requirement
+        {
+            if let Some(high) = permanent_requirement.high {
+                if on_return_value < permanent_requirement.low || on_return_value > high {
+                    println!("Function which raises IRQL to {} called from function with permanent requirement in interval {} to {}", on_return_value.value, permanent_requirement.low.value, high.value);
+                }
+            } else if on_return_value != permanent_requirement.low {
+                println!("Function which raises IRQL to {} called from function with permanent requirement {}", on_return_value.value, permanent_requirement.low.value);
+            }
         }
     }
 }
