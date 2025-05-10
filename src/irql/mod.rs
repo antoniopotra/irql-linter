257060131e97942333a14e54dd789aad02b4ca90
antoniopotra <antonio.potra@gmail.com>
2025-05-10 12:50:42 +0300

    Correct range check.

--- src/irql/mod.rs
+++ src/irql/mod.rs
@@ -49,17 +49,11 @@ impl IrqlRange {
     }
 
     pub fn contains(&self, irql_value: IrqlValue) -> bool {
-        if irql_value < self.low {
-            return false;
+        if let Some(high) = self.high {
+            self.low <= irql_value && irql_value <= high
+        } else {
+            irql_value == self.low
         }
-
-        if let Some(high) = self.high
-            && irql_value > high
-        {
-            return false;
-        }
-
-        true
     }
 }
 
